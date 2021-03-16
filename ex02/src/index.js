efb6f95d29676e1f9e0e415854c97311d7e863f3
technology512 <ald.wapper@gmail.com>
2021-03-16 22:58:18 +0100

    If Statements

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+
+function myFunction(myParameter) {
+
+    if (myParameter) {
+        return "The parameter is true!";
+      return "The parameter is true!";
+    }
+    return "The parameter is fales!";
+    return "The parameter is false!";
+  }
+  myFunction(true);
+  myFunction(fales);
+  console.log(myFunction(true));
+  console.log(myFunction(false));
+  // Only change code above this line
+  
+  module.exports = myFunction;
