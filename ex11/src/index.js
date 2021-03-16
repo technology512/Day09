9187b3b740229a411f333beca52fedd0289d5f6f
technology512 <ald.wapper@gmail.com>
2021-03-16 23:32:36 +0100

    switch statements

---
+++ ex11/src/index.js
@@ -0,0 +1,25 @@
+function switchCase(letter) {
+    // Write code below this line
+
+    var animals;
+    switch (letter) {
+        case "a":
+           animals = "antelope";
+            break;
+        case "b":
+           animals = "bird";
+            break;
+        case "c":
+            animals = "cat";
+            break;
+        default:
+            animals = "stuff";
+            break;
+    }
+    // Write code above this line
+    return animals;
+}
+
+
+console.log(switchCase("a"));
+module.exports = switchCase;
