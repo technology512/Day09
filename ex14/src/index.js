8e65ba74a89ab23748b786c37181ab99ae14128c
technology512 <ald.wapper@gmail.com>
2021-03-17 01:31:58 +0100

    For Loops

---
+++ ex14/src/index.js
@@ -0,0 +1,30 @@
+function myForLoop1() {
+    var evenNumbers = "";
+    for (var i = 0 ; i <= 8; i += 2) {
+        if (i != 8) {
+            evenNumbers += i + ", ";
+        } else {
+            evenNumbers += i;
+        }
+    }
+    return evenNumbers;
+}
+
+function myForLoop2() {
+    var evenInverseNumbers = "";
+    for (var i = 8 ; i >= 0; i -= 2) {
+        if (i != 0) {
+            evenInverseNumbers += i + ", ";
+        } else {
+            evenInverseNumbers += i;
+        }
+    }
+    return evenInverseNumbers;
+}
+
+console.log(myForLoop1());
+console.log(myForLoop2());
+module.exports = {
+    myForLoop1,
+    myForLoop2
+}; 
