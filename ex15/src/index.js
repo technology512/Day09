c8f5085b514603f2f7e905f42022dd73dedbd501
technology512 <ald.wapper@gmail.com>
2021-03-17 01:32:31 +0100

    Do...While Loops

---
+++ ex15/src/index.js
@@ -0,0 +1,17 @@
+function myDoWhile() {
+    var myNumbers = "";
+    var i = 0;
+    do {
+        if ((i != 9)) {
+            myNumbers += i + ", ";
+        } else {
+            myNumbers += i;
+        }
+        i++;
+    } while (i < 10);
+
+    return myNumbers;
+}
+
+console.log(myDoWhile());
+module.exports = myDoWhile;
